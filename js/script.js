172ed9ff39e31bf12a76354a8cb29e361ba7b7f7
ihmwhydn <ihm245@gmail.com>
2021-10-21 00:34:50 +0700

    add file

---
+++ js/script.js
@@ -0,0 +1,137 @@
+const swiper1 = new Swiper('.swiper1', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination1",
+        clickable: true,
+      },
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+      },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+      },
+});
+
+const swiper2 = new Swiper('.swiper2', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+      el: ".swiper-pagination2",
+      clickable: true,
+    },
+  autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+    },
+});
+
+const swiper3 = new Swiper('.swiper3', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+      el: ".swiper-pagination3",
+      clickable: true,
+    },
+  autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+    },
+});
+
+const swiper4 = new Swiper('.swiper4', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+      el: ".swiper-pagination4",
+      clickable: true,
+    },
+  autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+    },
+});
+
+const swiper5 = new Swiper('.swiper5', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+      el: ".swiper-pagination5",
+      clickable: true,
+    },
+  autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+    },
+});
+
+const swiper6 = new Swiper('.swiper6', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+      el: ".swiper-pagination6",
+      clickable: true,
+    },
+  autoplay: {
+      delay: 4500,
+      disableOnInteraction: false,
+    },
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+    },
+});
